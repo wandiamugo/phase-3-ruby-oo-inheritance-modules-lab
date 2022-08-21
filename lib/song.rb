600858a91ccc6cf0a3780b2b8b23f7a6fa221b40
wandiamugo <wwandia173@gmail.com>
2022-08-21 12:38:02 +0300

    commit tree 3132b1a57f1b55a66d6090f1cb9201b866cb39f3

--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,34 @@
-require 'pry'
+require "pry"
+require_relative "./concerns/memorable"
+require_relative "./concerns/findable"
+require_relative "./concerns/paramable"
 
 class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
+  include Paramable
+
+  extend Findable
+
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+    @@songs.detect { |a| a.name == name }
   end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
